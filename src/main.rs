b3b334b3fb4b1e8dc706ce12d92c35639597dd5d
Bill McCann <bill.mccann@gmail.com>
2024-04-25 09:29:49 -0400

    Converted stopwatch to system timer

--- src/main.rs
+++ src/main.rs
@@ -1,10 +1,10 @@
-extern crate stopwatch;
 extern crate project_euler;
+// extern crate stopwatch;
 
-use stopwatch::{Stopwatch};
-use std::io;
 use project_euler::euler_library::problems::*;
-use project_euler::euler_library::utilities;
+// use project_euler::euler_library::utilities;
+use std::io;
+use std::time::Instant;
 
 const LAST_PROBLEM: i32 = 13;
 
@@ -14,7 +14,8 @@ fn get_user_input() -> i32 {
     let mut problem_number = String::new();
 
     println!("\nWhat problem shall I run? Or type 'Q' to quit. ");
-    io::stdin().read_line(&mut problem_number)
+    io::stdin()
+        .read_line(&mut problem_number)
         .expect("Oops. Something went wrong when getting the problem number.");
 
     check_user_input(problem_number)
@@ -31,13 +32,14 @@ fn get_user_input() -> i32 {
 /// Parameter: "userInput", A string entered by the user.
 /// Returns: A valid integer (i32) problem number.
 fn check_user_input(user_input: String) -> i32 {
-
     let mut result: i32 = -1;
-    if user_input.trim().to_ascii_uppercase() == "Q" { return result }
+    if user_input.trim().to_ascii_uppercase() == "Q" {
+        return result;
+    }
 
     result = match user_input.trim().parse() {
         Ok(num) => num,
-        Err(_) => { 
+        Err(_) => {
             println!("\nSorry but I did not understand that. Please type the problem number or Q to quit.");
             get_user_input()
         }
@@ -47,43 +49,47 @@ fn check_user_input(user_input: String) -> i32 {
         println!("\nBTW, problem numbers are positive integers.");
         result = get_user_input();
     } else if result > LAST_PROBLEM {
-        println!("\nI have only completed problems 1 through {}", LAST_PROBLEM);
+        println!(
+            "\nI have only completed problems 1 through {}",
+            LAST_PROBLEM
+        );
         result = get_user_input();
     };
 
     result
-
 }
 
 /// run_problems is the main program logic, it uses a recursive loop to get input and
 /// select a problem number.
-/// 
+///
 /// Args
 ///    problem_number  The Project Euler problem number chosen by the user
-/// 
+///
 /// Returns
-///    The value zero 
+///    The value zero
 fn run_problem(problem_number: i32) -> i32 {
+    if problem_number < 1 {
+        return 0;
+    }
 
-    if problem_number < 1 { return 0 }
-
-    let mut total_time = Stopwatch::start_new();
+    let start = Instant::now();
     let result: String = problem_factory::get_solution(problem_number);
-    total_time.stop();
+    let duration = start.elapsed();
 
     println!("-----------------------------------------------------------------");
     println!("Solution to problem {} = {}", problem_number, result);
-    println!("Execution time was {}", utilities::format_milliseconds(total_time.elapsed_ms()));
+    println!(
+        "Execution time was {}",
+        duration.as_millis() // Converts and displays the time as milliseconds
+    );
     println!("-----------------------------------------------------------------");
 
     run_problem(get_user_input())
-
 }
 
 /// This is the main program
 fn main() {
-
+    println!("Welcome to Project Euler in Rust.");
     run_problem(get_user_input());
     println!("Goodbye");
-
 }
